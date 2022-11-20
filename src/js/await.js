efc799232bfddcb6dda26e5e5fb6d5b50b63ab40
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-11-20 11:42:59 -0500

    await Promise.all en una linea

--- src/js/await.js
+++ src/js/await.js
@@ -2,15 +2,21 @@ import { searchHeroAsync, searchHero } from './promises';
 
 const heroesIDs = ['cap', 'iron', 'spider'];
 
-export const getArrayHeroes = async () => {
-  const heroesArray = [];
+// export const getArrayHeroes = async () => {
+//   // const heroesArray = [];
 
-  for (const id of heroesIDs) {
-    // const hero = await searchHero(id);
-    // heroesArray.push(hero);
+//   // for (const id of heroesIDs) {
+//   //   // const hero = await searchHero(id);
+//   //   // heroesArray.push(hero);
 
-    heroesArray.push(searchHero(id))
-  }
+//   //   heroesArray.push(searchHero(id))
+//   // }
 
-  return await Promise.all(heroesArray);
-};
+//   // return await Promise.all(heroesArray);
+
+//   // Mejor forma:
+//   return await Promise.all(heroesIDs.map(searchHero));
+// };
+
+// También se puede resumir en una linea:
+export const getArrayHeroes = async () =>  await Promise.all(heroesIDs.map(searchHero));
